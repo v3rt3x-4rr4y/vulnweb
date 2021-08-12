9cae9bd02916ce5a79278187fe50bb372643b033
jukka@pwnlab <jukka@pwnlab.com>
2021-08-12 15:06:23 +0100

    Changed endpoint "role" parameter type from "path" to "request".

--- src/main/java/com/jukka/vulnweb/web/UserController.java
+++ src/main/java/com/jukka/vulnweb/web/UserController.java
@@ -12,6 +12,7 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -33,8 +34,8 @@ public class UserController
         return userRepository.findAll();
     }
 
-    @GetMapping("/role/{userRole}")
-    public List<User> findByRole(@PathVariable String userRole)
+    @GetMapping("/role")
+    public List<User> findByRole(@RequestParam String userRole)
     {
         return userRepository.findUsersByRole(userRole);
     }
